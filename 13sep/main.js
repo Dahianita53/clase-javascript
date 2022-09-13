194ac4ac78d76bc0f67641c528e9850118d9261f
carol castrillon <you@example.com>
2022-09-13 14:37:08 -0300

    clase de javascript

---
+++ 13sep/main.js
@@ -0,0 +1,45 @@
+// declara 4 variables y 1 constante
+
+const surname = `castrillon`
+
+let carrot = `zanahoria`
+
+let toy = `juguete`
+
+let house = `casa`
+
+let baby = `bebe`
+
+// tipo de datos
+
+let flag = true //boolean, dicotomica
+
+let number = 1 //number, numeros enteros o decimales
+
+let text = `el texto va entre comillas` //string, texto
+
+//arrays
+//cada elemento tiene un indice y parten desde 0
+
+let arr = [1, 3, 4, 8]
+
+let arr2 = ["texto", [1, 2, 3], 4, true] //arrays
+
+arr[2] // acceder al valor 4
+
+arr[2] = 5 // asignando un nuevo valor
+
+// para haceder al valor en un arreglo usamos: variableName [index] 
+
+let nothing = null //null representa un valor "vacio" o "nulo"
+
+let nodef = undefined
+
+let width // sin un valor asignado, esta variable es un "undefined"
+
+
+
+
+
+
+
